0ed79e04bb0e5a1785772e839a0ca1824c880cc5
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-08-01 09:48:04 +0000

    Initial commit

---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid!");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age can't be negative number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password"))
+        throw new Error(`Password can't contain "password"!`);
+    },
+  },
+});
+
+module.exports = User;
