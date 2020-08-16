38123c9c0a17cc66c96ca76a42907c9585e8ee0d
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-08-16 13:51:11 +0200

    accepting auth token

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is currently down. Please, check back soon!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -17,7 +29,9 @@ app.listen(port, () => {
 const jwt = require("jsonwebtoken");
 
 const myFunction = async () => {
-  const token = jwt.sign({ _id: "abasd" }, "thisisuniqueword", {expiresIn: '1 second'});
+  const token = jwt.sign({ _id: "abasd" }, "thisisuniqueword", {
+    expiresIn: "1 second",
+  });
 
   console.log(token);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -27,13 +28,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
